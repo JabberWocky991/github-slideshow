b2b3bf7e1cfd7458703ac301eaa786af15810fff
JabberWocky991 <67037876+JabberWocky991@users.noreply.github.com>
2021-10-01 15:35:45 +0930

    Add files via upload

---
+++ aws_instance.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+}
